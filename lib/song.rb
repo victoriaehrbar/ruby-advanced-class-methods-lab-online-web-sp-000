92e0356d74fa16e3f02f8998801a4625329d4399
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-05 21:19:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,4 +36,8 @@ def self.find_or_create_by_name(song_name)
   self.find_by_name(song_name) || self.create_by_name(song_name)
 end
 
+def self.alphabetical
+  self.all.sort_by{|s| s.name}
+end
+
 end
