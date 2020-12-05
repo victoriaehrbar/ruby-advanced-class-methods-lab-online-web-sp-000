1a3dbf0052a3b095da9676190308acc1717bde2f
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-05 21:20:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,30 @@ def self.alphabetical
   self.all.sort_by{|s| s.name}
 end
 
-def self.new_from_filename
+def self.new_from_filename(filename)
+   parts = filename.split(" - ")
+   artist_name = parts[0]
+   song_name = parts[1].gsub(".mp3", "")
+
+   song = self.new
+   song.name = song_name
+   song.artist_name = artist_name
+   song
+ end
+
+ def self.create_from_filename(filename)
+   parts = filename.split(" - ")
+   artist_name = parts[0]
+   song_name = parts[1].gsub(".mp3", "")
+
+   song = self.create
+   song.name = song_name
+   song.artist_name = artist_name
+   song
+ end
+
+ def self.destroy_all
+   self.all.clear
+ end
 
 end
