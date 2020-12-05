843423fd71ef55df82fda78f4212b5617aca7169
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-05 21:17:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,4 +16,16 @@ class Song
      song
    end
 
+   def self.new_by_name(song_name)
+     song = self.new
+     song.name = song_name
+     song
+   end
+
+   def self.create(song_name)
+  song = self.create
+  song.name = song_name
+  song
+end
+
 end
