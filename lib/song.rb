c3960eefc4f78da5619943a0942756980840c49a
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-05 21:19:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,4 +28,12 @@ class Song
     song
   end
 
+  def self.find_by_name(song_name)
+  self.all.detect{|s| s.name == song_name}
+end
+
+def self.find_or_create_by_name(song_name)
+  self.find_by_name(song_name) || self.create_by_name(song_name)
+end
+
 end
